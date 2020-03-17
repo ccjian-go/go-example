2aa0bb3272ea45ca5360f2b81c2a82cc127ec4e1
ccjian <377148474@qq.com>
2020-03-17 10:00:02 +0800

    1

--- 16.1.Struct.go
+++ 16.1.Struct.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"go-example/structs/computer"
+)
 
 /*
 	一. 结构体是用户定义的类型，表示若干个字段（Field）的集合。
@@ -24,6 +27,13 @@ import "fmt"
 		Person 结构体有一个匿名字段 Address，而 Address 是一个结构体。
 		现在结构体 Address 有 city 和 state 两个字段，
 		访问这两个字段就像在 Person 里直接声明的一样，因此我们称之为提升字段。
+	十一. 如果结构体名称以大写字母开头，则它是其他包可以访问的导出类型（Exported Type）。
+		同样，如果结构体里的字段首字母大写，它也能被其他包访问到。
+	十二. 结构体是值类型。如果它的每一个字段都是可比较的，则该结构体也是可比较的。
+		如果两个结构体变量的对应字段相等，则这两个变量也是相等的。
+		如果结构体包含不可比较的字段，则结构体变量也不可比较。
+	十三. 结构体类型 image 包含一个 map 类型的字段。
+		由于 map 类型是不可比较的，因此 image1 和 image2 也不可比较。
 
  */
 type Employee struct {
@@ -73,6 +83,14 @@ type Spec struct { //exported struct
 	Price int //exported field
 }
 
+type name struct {
+	firstName string
+	lastName string
+}
+
+type image struct {
+	data map[int]int
+}
 
 func main() {
 	//creating structure using field names
@@ -166,6 +184,35 @@ func main() {
 	var spec computer.Spec
 	spec.Maker = "apple"
 	spec.Price = 50000
+	//spec.model = "Mac Mini"
 	fmt.Println("Spec:", spec)
+
+
+	name1 := name{"Steve", "Jobs"}
+	name2 := name{"Steve", "Jobs"}
+	if name1 == name2 {
+		fmt.Println("name1 and name2 are equal")
+	} else {
+		fmt.Println("name1 and name2 are not equal")
+	}
+
+	name3 := name{firstName:"Steve", lastName:"Jobs"}
+	name4 := name{}
+	name4.firstName = "Steve"
+	if name3 == name4 {
+		fmt.Println("name3 and name4 are equal")
+	} else {
+		fmt.Println("name3 and name4 are not equal")
+	}
+
+	//image1 := image{data: map[int]int{
+	//	0: 155,
+	//}}
+	//image2 := image{data: map[int]int{
+	//	0: 155,
+	//}}
+	//if image1 == image2 {
+	//	fmt.Println("image1 and image2 are equal")
+	//}
 }
 
